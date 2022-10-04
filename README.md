b64cc6912a284febc72c1e2f4007cb663f0fe5e9
Kyrylo Petrov <petrovk@info.local>
2022-10-04 10:27:17 +0200

    Init

---
+++ README.md
@@ -0,0 +1,22 @@
+# uMario Enhanced
+<p align="center"><img src="./screenshots/main-menu.png"></p>
+
+uMario Vita port ported back to PC with additional features.
+
+## Controls
+- <kbd>Arrows</kbd> and <kbd>WASD</kbd> - controls in menu.
+- <kbd>WASD</kbd> - controls in game.
+- <kbd>Space</kbd> - jumping in game.
+- <kbd>Shift</kbd> - running in game.
+- <kbd>ESC</kbd> - enter menu in game and dismiss it.
+- <kbd>Enter</kbd> - accept in menu.
+
+## Known issues 
+1. Sometimes player becomes invincible and stays like that until restarting game or taking a star.
+2. Sometimes some sounds are loud.
+3. If you exit from or die in levels with fixed camera, camera will remain fixed until you exit the game.
+4. Sometimes player spawns higher than they should (this allows player to climb at unavailable area right after spawn).
+5. Due to wider screen you can see parts of levels that you normally shouldn't.
+
+## Distributing
+After build, in the distributing folder you need to include executable (uMario.exe), contents of "bin" directory (*.dll) and directory "files" itself. After that, "uMario.exe" will run on every Windows computer.
---
+++ uNext/LoadingMenu.cpp
@@ -0,0 +1,65 @@
+#include "LoadingMenu.h"
+#include "CFG.h"
+#include "Core.h"
+
+/* ******************************************** */
+
+LoadingMenu::LoadingMenu(void) {
+	this->iTime = 0;
+	this->loadingType = true;
+}
+
+LoadingMenu::~LoadingMenu(void) {
+
+}
+
+/* ******************************************** */
+
+void LoadingMenu::Update() {
+	if(SDL_GetTicks() >= iTime + 2500 + (loadingType ? 0 : 2750)) {
+		if(loadingType) {
+			if(!CCore::getMap()->getInEvent()) {
+				CCore::getMap()->loadLVL();
+				CCore::getMap()->setSpawnPoint();
+			}
+			CCFG::getMM()->setViewID(CCFG::getMM()->eGame);
+			CCFG::getMusic()->changeMusic(true, true);
+		} else {
+			CCore::getMap()->resetGameData();
+			CCFG::getMM()->setViewID(CCFG::getMM()->eMainMenu);
+		}
+	} else {
+		CCFG::getMusic()->StopMusic();
+	}
+	CCore::getMap()->UpdateBlocks();
+}
+
+void LoadingMenu::Draw(SDL_Renderer* rR) {
+	const int halfVitaWidth = (960-800) / 2;
+	const int halfVitaHeight = (540-448) / 2;
+	
+	if(loadingType) {
+		CCore::getMap()->DrawGameLayout(rR);
+		CCFG::getText()->Draw(rR, "WORLD", 320 + halfVitaWidth, 144 + halfVitaHeight);
+		CCFG::getText()->Draw(rR, CCore::getMap()->getLevelName(), 416 + halfVitaWidth, 144 + halfVitaHeight);
+
+		CCore::getMap()->getPlayer()->getMarioSprite()->getTexture()->Draw(rR, 342 + halfVitaWidth, 210 - CCore::getMap()->getPlayer()->getHitBoxY()/2 + halfVitaHeight);
+
+		CCFG::getText()->Draw(rR, "y", 384 + halfVitaWidth, 208 + halfVitaHeight);
+
+		if(CCore::getMap()->getPlayer()->getNumOfLives() > 9) {
+			CCore::getMap()->getBlock(180)->getSprite()->getTexture()->Draw(rR, 410 + halfVitaWidth, 210 + halfVitaHeight);
+		}
+		CCFG::getText()->Draw(rR, std::to_string(CCore::getMap()->getPlayer()->getNumOfLives()), 432 + halfVitaWidth, 208 + halfVitaHeight);
+		CCFG::getText()->DrawCenterX(rR, "REMEMBER THAT YOU CAN RUN WITH " + CCFG::getKeyString(CCFG::keyIDShift), 400 + halfVitaHeight, 16);
+	} else {
+		CCore::getMap()->DrawGameLayout(rR);
+		CCFG::getText()->DrawCenterX(rR, "GAME OVER", 240 + halfVitaHeight, 16);
+	}
+}
+
+/* ******************************************** */
+
+void LoadingMenu::updateTime() {
+	this->iTime = SDL_GetTicks();
+}
